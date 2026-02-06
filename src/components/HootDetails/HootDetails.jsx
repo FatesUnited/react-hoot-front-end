23510424550bc6a2e1d963a5759a2c5ecd7edd8c
Thomas Phillips <fatesunited@gmail.com>
2026-02-06 15:02:22 -0600

    finished core hoot front end lecture

---
+++ src/components/HootDetails/HootDetails.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect, useContext } from 'react';
+import { useParams, Link } from 'react-router';
+import CommentForm from '../CommentForm/CommentForm';
+import * as hootService from '../../services/hootService';
+import { UserContext } from '../../contexts/UserContext';
+
+const HootDetails = (props) => {
+    const { hootId } = useParams();
+    const { user } = useContext(UserContext);
+    const [hoot, setHoot] = useState(null);
+
+     useEffect(() => {
+        const fetchHoot = async () => {
+        const hootData = await hootService.show(hootId);
+        setHoot(hootData);
+        };
+        fetchHoot();
+    }, [hootId]);
+
+    // Verify the hoot state is set correctly:
+    console.log('hoot state:', hoot);
+
+    if (!hoot) return <main>Loading...</main>;
+
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await hootService.createComment(hootId, commentFormData);
+    setHoot({ ...hoot, comments: [...hoot.comments, newComment] });
+  };
+
+  const handleDeleteComment = async (commentId) => {
+    const deletedComment = await hootService.deleteComment(hootId, commentId);
+
+    if(deletedComment.err) throw new Error(deletedComment.err);
+
+    setHoot({
+      ...hoot,
+      comments: hoot.comments.filter((comment) => comment._id !== commentId),
+    });
+  };
+
+    return (
+    <main>
+      <section>
+        <header>
+          <p>{hoot.category.toUpperCase()}</p>
+          <h1>{hoot.title}</h1>
+          <p>
+            {`${hoot.author.username} posted on
+            ${new Date(hoot.createdAt).toLocaleDateString()}`}
+          </p>
+          {hoot.author._id === user._id && (
+          <>
+            <Link to={`/hoots/${hootId}/edit`}>Edit</Link>
+
+            <button onClick={() => props.handleDeleteHoot(hootId)}>
+              Delete
+            </button>
+          </>
+        )}
+        </header>
+        <p>{hoot.text}</p>
+      </section>
+      <section>
+        <h2>Comments</h2>
+        <CommentForm handleAddComment={handleAddComment}/>
+           
+        {!hoot.comments.length && <p>There are no comments.</p>}
+
+        {hoot.comments.map((comment) => (
+          <article key={comment._id}>
+            <header>
+              <p>
+                {`${comment.author.username} posted on
+                ${new Date(comment.createdAt).toLocaleDateString()}`}
+              </p>
+            </header>
+            <p>{comment.text}</p>
+            <Link to={`/hoots/${hootId}/comments/${comment._id}/edit`}><button> Edit Comment </button></Link>
+            {comment.author._id === user._id && (
+                <button onClick={() => handleDeleteComment(comment._id)}>
+                    Delete Comment
+                </button>
+            )}
+          </article>
+        ))}
+      </section>
+    </main>
+  );
+};
+
+export default HootDetails;
